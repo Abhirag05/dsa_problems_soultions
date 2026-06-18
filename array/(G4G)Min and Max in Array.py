63ea876ad9eb978320a1fcf869561c19d29c6ada
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-06-18 11:48:13 +0530

    feat: add solution of qn from G4G Min and Max in Array problem

---
+++ array/(G4G)Min and Max in Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getMinMax(self, arr):
+        # code here
+        return [min(arr),max(arr)]
+
+#Logic: We can use the built-in min() and max() functions to find the minimum and maximum elements in the array. 
+#Time Complexity: O(n)
+#Space Complexity: O(1)
